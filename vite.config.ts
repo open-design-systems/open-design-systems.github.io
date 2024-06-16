68e8a3ad7dd5172e38e7f5af748f071f82092f80
Marcos Bérgamo <marcosvinicius_0305@hotmail.com>
2024-06-16 13:16:34 +0000

    fix: page title and correct path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<REPO>/",
+  base: "/open-design-system/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
